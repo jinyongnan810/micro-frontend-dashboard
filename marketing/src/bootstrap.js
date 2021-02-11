ee8f4ffa8660c3ace18db7ed6ed6fa7effa07bb6
Yuunan Kin <yuunan.kin@gmail.com>
2021-02-12 08:49:45 +0900

    react markups in marking

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 const mount = (el) => {
-  ReactDOM.render(<h1>Markerting Page</h1>, el);
+  ReactDOM.render(<App />, el);
 };
 
 if (process.env.NODE_ENV === "development") {
