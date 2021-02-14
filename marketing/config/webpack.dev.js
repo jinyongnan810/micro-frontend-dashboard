ec5a740baaafbbad4b7c390e778f5b9324333073
Yuunan Kin <yuunan.kin@gmail.com>
2021-02-14 11:33:25 +0900

    production config

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJSON = require("../package.json");
@@ -19,9 +19,6 @@ const devConfig = {
         "./MarketingApp": "./src/bootstrap",
       },
       shared: packageJSON.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
