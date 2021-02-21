3fb6c458265dc308867d67d1cbd60befcb7c1af5
Yuunan Kin <yuunan.kin@gmail.com>
2021-02-21 10:10:29 +0900

    commit

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -4,7 +4,7 @@ import App from "./App";
 const mount = (el) => {
   ReactDOM.render(<App />, el);
 };
-
+// render
 if (process.env.NODE_ENV === "development") {
   const el = document.getElementById("_dev-marketing-root");
   if (el) {
