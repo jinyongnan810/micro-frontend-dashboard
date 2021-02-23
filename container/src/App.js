e18af9abb2d175f45bc6c67159b4225ece28c5db
Yuunan Kin <yuunan.kin@gmail.com>
2021-02-23 09:22:17 +0900

    fix css

--- container/src/App.js
+++ container/src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
