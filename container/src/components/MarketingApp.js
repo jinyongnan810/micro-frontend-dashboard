2f0663aa371aa0a11f0ccf2bbc77757de5cb5071
Yuunan Kin <yuunan.kin@gmail.com>
2021-02-24 08:59:58 +0900

    sub->parent sync

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
 import { mount } from "marketing/MarketingApp";
 const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current, {
+      onNavigate: (location) => {
+        history.push(location.pathname);
+      },
+    });
   }, []);
   return <div ref={ref} />;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createMemoryHistory } from "history";
 import App from "./App";
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
+  history.listen(onNavigate);
   ReactDOM.render(<App history={history} />, el);
 };
 // render
