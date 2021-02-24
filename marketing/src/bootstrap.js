45d00089909db276e7b65e35a267f8f6cabc7098
Yuunan Kin <yuunan.kin@gmail.com>
2021-02-24 09:05:04 +0900

    prevent error in isolation

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -4,14 +4,17 @@ import { createMemoryHistory } from "history";
 import App from "./App";
 const mount = (el, { onNavigate }) => {
   const history = createMemoryHistory();
-  history.listen(onNavigate);
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(<App history={history} />, el);
 };
 // render
 if (process.env.NODE_ENV === "development") {
   const el = document.getElementById("_dev-marketing-root");
   if (el) {
-    mount(el);
+    mount(el, {});
   }
 }
 export { mount };
